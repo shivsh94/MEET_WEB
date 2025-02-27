e2971aa2b3464809effd2498321cd5579a11ae1d
shivsh94 <shivamsharma23jan@gmail.com>
2025-02-27 15:41:31 +0530

    Update CORS configuration to use environment variable for client URL

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 const server = createServer(app);
 export const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["process.env.CLIENT_URL"],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["CLIENT_URL", "http://localhost:3000"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE"],
